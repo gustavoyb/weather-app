a879b5fe824dd756d05051caf30eef413c08feaa
Gustavo Ybañez <gustavoybannez@gmail.com>
2022-01-28 10:25:35 -0300

    se crea la todays weather card

---
+++ src/components/TodaysWeather.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TodaysWeather.css';
+
+export const TodaysWeather = () => {
+    return (
+        <div className='TodaysWeather'>
+            
+        </div>
+    )
+}
